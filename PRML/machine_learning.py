9f9962803c647c073c9215ac360942dbc6b1676e
Matthew <greenchicken1902@gmail.com>
2022-08-30 20:39:44 +1000

    Added todo comments Added an EDA Moved the EDA figs from processData to exploratoryDataAnalysis Minor changes to docstrings and loggings

--- PRML/machine_learning.py
+++ PRML/machine_learning.py
@@ -19,8 +19,6 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.decomposition import PCA
 
-DataFrame: type = pd.core.frame.DataFrame
-
 
 class MachineLearning(object):
 
@@ -42,11 +40,12 @@ class MachineLearning(object):
 
     def bunchToDataframe(self, fetched_dataset: Bunch) -> DataFrame:
         """
-        Creates a pandas DataFrame dataset from SKLearn Bunch object.
+        Creates a pandas DataFrame dataset from the SKLearn Bunch object.
         :param fetched_dataset: Bunch
         :return:
             - dataset - DataFrame
         """
+        logging.info('Converting Bunch to DataFrame')
         dataset = pd.DataFrame(data=fetched_dataset['data'], columns=fetched_dataset['feature_names'])
         dataset[self.config.target] = fetched_dataset['target']
         return dataset
@@ -59,39 +58,36 @@ class MachineLearning(object):
         :return:
             - dataset - DataFrame
         """
-        logging.info(f"Loading dataset")
+        logging.info(f'Loading dataset {self.config.dataset_name}')
 
         if self.config.dataset_type == 'openml':
             if not os.path.isfile(self.config.dataset_dir):
                 try:
                     fetched_dataset = fetch_openml(self.config.dataset_name, version=1)
                 except Exception as e:
-                    print(e)
+                    logging.warning(e)
                     return
                 logging.info('Fetched dataset')
                 dataset = self.bunchToDataframe(fetched_dataset)
                 self.config.names = fetched_dataset['feature_names'] + [self.config.target]
                 self.config.dataset_type = '.csv'
-                logging.info('Converted dataset to DataFrame')
                 self.saveDataset(dataset)
-
         try:
             dataset = pd.read_csv(self.config.dataset_dir, names=self.config.names, sep=self.config.seperator)
         except FileNotFoundError as e:
             logging.warning(e)
             return
-        logging.info('Loaded dataset')
         return dataset
 
     def saveDataset(self, dataset: DataFrame) -> None:
         """
-        Saves the dataset to a file using pandas to csv.
+        Saves the dataset to a csv file using pandas.
         :param dataset: DataFrame
         :return:
             - None
         """
+        logging.info(f'Saving dataset {self.config.dataset_dir}')
         dataset.to_csv(self.config.dataset_dir, sep=self.config.seperator, index=False)
-        logging.info('Dataset saved', self.config.dataset_dir)
 
     def processData(self, dataset: DataFrame) -> tuple:
         """
@@ -100,18 +96,53 @@ class MachineLearning(object):
         :return:
             - dataset, X, y - tuple[DataFrame]
         """
-        X, y = dataset.drop(self.config.target, axis=1), dataset[self.config.target]
+        # TODO: Fix docstring
+        logging.info('Processing data')
+        X = dataset.drop(self.config.target, axis=1)  # denotes independent features
+        y = dataset[self.config.target]  # denotes dependent variables
 
-        plt.figure(figsize=(10, 3))
-        for i in range(5):
-            instance_data = dataset.iloc[i]
-            image = instance_data[:-1].values
-            label = instance_data[-1]
-            plt.subplot(1, 5, i + 1)
-            plt.imshow(np.array(image).reshape(28, 28), cmap=plt.cm.gray)
-            plt.title('Training: %i\n' % int(label), fontsize=15)
-        plt.plot()
-        logging.info('Example instances')
+        if self.config.show_small_responses:
+            print(dataset.axes)
+            print(dataset.head())
+            print(dataset.isnull().sum())  # check for missing values
+            print(dataset.dtypes)
+            if self.config.dataset_name == 'Fashion-MNIST':
+                print(np.array(X.iloc[9].values).reshape(28, 28))
+            print('X shape:', X.shape)
+        return dataset, X, y
+
+    def extractFeatures(self, dataset: DataFrame, x: DataFrame, y: DataFrame) -> tuple:
+        """
+
+        :param dataset: DataFrame
+        :param x: DataFrame
+        :param y: DataFrame
+        :return:
+            - dataset, X, y - tuple[DataFrame]
+        """
+        # TODO: Fix docstring
+        #   Add extraction methods
+        logging.info('Extracting features')
+        X = x  # denotes independent features
+
+        return dataset, X, y
+
+    def exploratoryDataAnalysis(self, dataset: DataFrame, x: DataFrame, y: DataFrame) -> None:
+        """
+
+        :param dataset: DataFrame
+        :param x: DataFrame
+        :param y: DataFrame
+        :return:
+            - None
+        """
+        # TODO: Fix docstring
+        #   Add legend to scatter plot
+        logging.info('Exploratory Data Analysis')
+        if not self.config.show_figs:
+            return
+
+        X = x  # denotes independent features
 
         plt.figure()
         dataset[self.config.target].value_counts().plot(kind='bar')
@@ -122,52 +153,75 @@ class MachineLearning(object):
         plt.scatter(view[:, 0], view[:, 1], c=y, alpha=0.2, cmap='Set1')
         plt.plot()
 
-        plt.figure(figsize=(15, 3))
-        for i in range(10):
-            for idx in range(dataset.shape[0]):
-                instance_data = dataset.iloc[idx]
+        if self.config.dataset_name == 'Fashion-MNIST':
+            plt.figure(figsize=(10, 3))
+            for i in range(5):
+                instance_data = dataset.iloc[i]
+                image = instance_data[:-1].values
                 label = instance_data[-1]
-                if label == i:
-                    image = instance_data[:-1].values
-                    plt.subplot(1, 10, i + 1)
-                    plt.imshow(np.array(image).reshape(28, 28), cmap=plt.cm.gray)
-                    plt.title('Label: %i\n' % int(i), fontsize=15)
-                    break
-        plt.plot()
+                plt.subplot(1, 5, i + 1)
+                plt.imshow(np.array(image).reshape(28, 28), cmap=plt.cm.gray)
+                plt.title('Training: %i\n' % int(label), fontsize=15)
+            plt.plot()
+
+            plt.figure(figsize=(15, 3))
+            for i in range(10):
+                for idx in range(dataset.shape[0]):
+                    instance_data = dataset.iloc[idx]
+                    label = instance_data[-1]
+                    if label == i:
+                        image = instance_data[:-1].values
+                        plt.subplot(1, 10, i + 1)
+                        plt.imshow(np.array(image).reshape(28, 28), cmap=plt.cm.gray)
+                        plt.title('Label: %i\n' % int(i), fontsize=15)
+                        break
+            plt.plot()
 
         plt.show()
 
-        if self.config.show_small_responses:
-            print(dataset.axes)
-            print(dataset.head())
-            print(dataset.dtypes)
+    def splitDataset(self, x: DataFrame, y: DataFrame) -> tuple:
+        """
 
-            print(np.array(X.iloc[9][:-1].values.reshape(28, 28)))
-        print('X shape:', X.shape)
-        logging.info('Data processed')
-        return dataset, X, y
-
-    def extractFeatures(self, dataset: DataFrame) -> tuple:
-        X, y = dataset.drop(self.config.target, axis=1), dataset[self.config.target]
-
-        logging.info('Features extracted')
-        return dataset, X, y
-
-    def splitDataset(self, x, y) -> tuple:
+        :param x: DataFrame
+        :param y: DataFrame
+        :return:
+            - X_train, X_test, y_train, y_test - tuple[DataFrame]
+        """
+        # TODO: Fix docstring
+        logging.info('Splitting data')
         X_train, X_test, y_train, y_test = train_test_split(x, y, train_size=self.config.split_ratio,
                                                             random_state=self.config.random_seed)
-        logging.info('Dataset split')
         return X_train, X_test, y_train, y_test
 
-    def trainModel(self, x_train, y_train):
+    def trainModel(self, x_train: DataFrame, y_train: DataFrame) -> Any:
+        """
+
+        :param x_train: DataFrame
+        :param y_train: DataFrame
+        :return:
+            - model - Any
+        """
+        # TODO: Fix docstring
+        #   Add other ML techniques
+        logging.info('Training model')
         model = None
         if self.config.model_type == 'LogisticRegression':
             model = LogisticRegression(solver='lbfgs', max_iter=100)
-            model.fit(x_train, y_train)
-        return model
 
-    @staticmethod
-    def resultAnalysis(model, x, x_test, y_test):
+        return model.fit(x_train, y_train)
+
+    def resultAnalysis(self, model: Any, x: DataFrame, x_test: DataFrame, y_test: DataFrame) -> None:
+        """
+
+        :param model: Any
+        :param x: DataFrame
+        :param x_test: DataFrame
+        :param y_test: DataFrame
+        :return:
+            - None
+        """
+        # TODO: Fix docstring
+        logging.info('Analysing results')
         score = model.score(x_test, y_test)
         print("Score - %.4f%s" % (score * 100, '%'))
 
@@ -175,37 +229,40 @@ class MachineLearning(object):
 
         cm = confusion_matrix(y_test, y_pred)
 
-        ConfusionMatrixDisplay(cm, display_labels=model.classes_).plot()
+        if self.config.show_figs:
+            ConfusionMatrixDisplay(cm, display_labels=model.classes_).plot()
 
-        plt.figure(figsize=(10, 3))
-        for i in range(5):
-            prediction = model.predict(x)[i]
-            plt.subplot(1, 5, i + 1)
-            plt.axis("off")
-            image = x.iloc[[i]].values[0]
-            plt.imshow(np.array(image).reshape(28, 28), cmap=plt.cm.gray_r, interpolation='nearest')
-            plt.title('Prediction: %i' % int(prediction))
-        plt.plot()
-        logging.info('Predicted results')
+            plt.figure(figsize=(10, 3))
+            for i in range(5):
+                prediction = model.predict(x)[i]
+                plt.subplot(1, 5, i + 1)
+                plt.axis("off")
+                image = x.iloc[[i]].values[0]
+                plt.imshow(np.array(image).reshape(28, 28), cmap=plt.cm.gray_r, interpolation='nearest')
+                plt.title('Prediction: %i' % int(prediction))
+            plt.plot()
+            logging.info('Predicted results')
 
-        index = 0
-        misclassifiedIndexes = []
-        for label, predict in zip(y_test, y_pred):
-            if label != predict:
-                misclassifiedIndexes.append(index)
-            index += 1
+            index = 0
+            misclassifiedIndexes = []
+            for label, predict in zip(y_test, y_pred):
+                if label != predict:
+                    misclassifiedIndexes.append(index)
+                index += 1
 
-        plt.figure(figsize=(15, 3))
-        for plotIndex, badIndex in enumerate(misclassifiedIndexes[0:5]):
-            plt.subplot(1, 5, plotIndex + 1)
-            plt.axis("off")
-            image = x_test.iloc[[badIndex]].values[0]
-            plt.imshow(np.array(image).reshape(28, 28),
-                       cmap=plt.cm.gray, interpolation='nearest')
-            plt.title('Predicted: {}, Actual: {}'.format(y_pred[badIndex], np.array(y_test)[badIndex]),
-                      fontsize=15)
-        plt.plot()
-        logging.info('Misclassified results')
+            plt.figure(figsize=(15, 3))
+            for plotIndex, badIndex in enumerate(misclassifiedIndexes[0:5]):
+                plt.subplot(1, 5, plotIndex + 1)
+                plt.axis("off")
+                image = x_test.iloc[[badIndex]].values[0]
+                plt.imshow(np.array(image).reshape(28, 28),
+                           cmap=plt.cm.gray, interpolation='nearest')
+                plt.title('Predicted: {}, Actual: {}'.format(y_pred[badIndex], np.array(y_test)[badIndex]),
+                          fontsize=15)
+            plt.plot()
+            logging.info('Misclassified results')
+
+            plt.show()
 
         print(classification_report(y_test, y_pred))
 
@@ -213,8 +270,6 @@ class MachineLearning(object):
         print('Mean Absolute Error:', mean_absolute_error(y_test, y_pred))
         print('Mean Squared Error:', mean_squared_error(y_test, y_pred))
         print('Mean Root Squared Error:', np.sqrt(mean_squared_error(y_test, y_pred)))
-        plt.show()
-        logging.info('Results complete')
 
     def saveModel(self, model: Any) -> None:
         """
@@ -228,7 +283,7 @@ class MachineLearning(object):
 
     def loadModel(self) -> Any:
         """
-        Loads the model by deserialising a json file
+        Loads the model by deserialising a model file.
         :return:
             - model - Any
         """
@@ -244,7 +299,10 @@ class MachineLearning(object):
             return
 
         self.dataset, self.X, self.y = self.processData(self.raw_dataset)
-        self.dataset, self.X, self.y = self.extractFeatures(self.dataset)
+
+        self.dataset, self.X, self.y = self.extractFeatures(self.dataset, self.X, self.y)
+
+        self.exploratoryDataAnalysis(self.dataset, self.X, self.y)
 
         self.X_train, self.X_test, self.y_train, self.y_test = self.splitDataset(self.X, self.y)
 
@@ -255,5 +313,3 @@ class MachineLearning(object):
         self.saveModel(self.model)
         self.model = self.loadModel()
         self.resultAnalysis(self.model, self.X, self.X_test, self.y_test)
-
-        logging.info('Competed')
