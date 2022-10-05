d5983fba66e1943c6f9dc362dd2f128c962e635f
Matthew <greenchicken1902@gmail.com>
2022-10-05 14:41:42 +1100

    Added warnings and ignore or raise options Added an update method, which updated objects attributes Minor changes

--- machine_learning/utils.py
+++ machine_learning/utils.py
@@ -4,6 +4,7 @@ import json
 import logging
 import os
 import pickle
+import warnings
 from typing import Any
 
 
@@ -27,8 +28,8 @@ def joinPath(path_: str, *paths, ext: str = '') -> str:
     Joins the paths together, adds an extension if not already included
     in path.
 
-    :param path_: main file path, should be a str
-    :param paths: remaining file paths, should be a tuple[str]
+    :param path_: Main file path, should be a str
+    :param paths: Remaining file paths, should be a tuple[str]
     :param ext: file extension, should be a str
     :return: path_ - str
     """
@@ -42,8 +43,8 @@ def makePath(path_: str, *paths) -> str:
     """
     Checks if the path exists and creates the path when required.
 
-    :param path_: main file path, should be a str
-    :param paths: remaining file paths, should be a tuple[str]
+    :param path_: Main file path, should be a str
+    :param paths: Remaining file paths, should be a tuple[str]
     :return: path_ - str
     """
     path_, exist = checkPath(path_, *paths)
@@ -52,23 +53,53 @@ def makePath(path_: str, *paths) -> str:
     return path_
 
 
-def load(dir_: str, name: str) -> Any:
+def update(obj: object, kwargs: dict) -> object:
+    """
+    Updates the objects attributes, if given attributes are present
+    in object and match existing data types.
+
+    :param obj: The object that is being updated, should be an object
+    :param kwargs: Keywords and values to be updated, should be a dict
+    :return: obj - object
+    """
+    for key, value in kwargs.items():
+        if not hasattr(obj, key):
+            raise AttributeError(f"'{obj.__class__.__name__}' object has no attribute '{key}'")
+        else:
+            attr_ = getattr(obj, key)
+            if isinstance(attr_, (type(value), type(None))):
+                setattr(obj, key, value)
+            else:
+                raise AttributeError(f"'{key}': got '{type(value).__name__}' but expected type is "
+                                     f"'{type(attr_).__name__}'")
+    return obj
+
+
+def load(dir_: str, name: str, errors: str = 'raise') -> Any:
     """
     Loads the data with appropriate method. Pickle will deserialise the
     contents of the file and json will load the contents.
 
-    :param dir_: directory of file, should be a str
-    :param name: name of file, should be a str
+    :param dir_: Directory of file, should be a str
+    :param name: Name of file, should be a str
+    :param errors: If 'ignore', suppress errors, should be str
     :return: data - Any
     """
+    if errors not in ["ignore", "raise"]:
+        raise ValueError("The parameter errors must be either 'ignore' or 'raise'")
+
     path_, exist = checkPath(dir_, name)
     ext = os.path.splitext(name)[1]
 
     if not exist:
         logging.warning(f"Path '{path_}' does not exist")
+        if errors == 'raise':
+            warnings.warn(f"Path '{path_}' does not exist")
         return
     if not ext:
         logging.warning(f"Name '{name}' must include file extension")
+        if errors == 'raise':
+            warnings.warn(f"Name '{name}' must include file extension")
         return
 
     if ext == '.json':
@@ -80,26 +111,34 @@ def load(dir_: str, name: str) -> Any:
     return data
 
 
-def save(dir_: str, name: str, data: Any, indent: int = 4) -> bool:
+def save(dir_: str, name: str, data: Any, indent: int = 4, errors: str = 'raise') -> bool:
     """
     Saves the data with appropriate method. Pickle will serialise the
     object, while json will dump the data with indenting to allow users
     to edit and easily view the encoded data.
 
-    :param dir_: directory path of file, should be a str
-    :param name: name of file, should be a str
-    :param data: data to be saved, should be an Any
-    :param indent: data's indentation within the file, should be an int
+    :param dir_: Directory of file, should be a str
+    :param name: Name of file, should be a str
+    :param data: Data to be saved, should be an Any
+    :param indent: Data's indentation within the file, should be an int
+    :param errors: If 'ignore', suppress errors, should be str
     :return: completed - bool
     """
+    if errors not in ["ignore", "raise"]:
+        raise ValueError("The parameter errors must be either 'ignore' or 'raise'")
+
     path_ = joinPath(dir_, name)
     ext = os.path.splitext(name)[1]
 
     if not os.path.exists(dir_):
         logging.warning(f"Path '{dir_}' does not exist")
+        if errors == 'raise':
+            warnings.warn(f"Path '{dir_}' does not exist")
         return False
     if not ext:
         logging.warning(f"File '{name}' must include file extension in name")
+        if errors == 'raise':
+            warnings.warn(f"File '{name}' must include file extension in name")
         return False
 
     if ext == '.json':
@@ -109,6 +148,8 @@ def save(dir_: str, name: str, data: Any, indent: int = 4) -> bool:
         pickle.dump(data, open(path_, "wb"))
     else:
         logging.warning(f"Saving method was not determined, failed to save file")
+        if errors == 'raise':
+            warnings.warn(f"Saving method was not determined, failed to save file")
         return False
     logging.info(f"File '{name}' was saved")
     return True
