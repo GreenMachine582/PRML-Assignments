ea2de1bf48cdfda694edd5af2af9f08bd1d5e1ab
Matthew <greenchicken1902@gmail.com>
2022-10-07 12:20:23 +1100

    Minor changes Generalised convertToCategorical

--- examples/classifier.py
+++ examples/classifier.py
@@ -1,42 +1,44 @@
 from __future__ import annotations
 
 import logging
-import os
 
 from matplotlib import pyplot as plt
 from numpy import ndarray
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from seaborn import heatmap
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
-# Constants
-local_dir = os.path.dirname(__file__)
 
-
-def convertToCategorical(df: DataFrame) -> DataFrame:
+def convertToCategorical(df: DataFrame, target: str) -> DataFrame:
     """
-    Encodes the BTC Target into a binary category where,
-    0 represents a fall in price from previous instance
-    1 represents a rise in price from previous instance.
+    Encodes the target into a binary category where,
+    0 represents a decrease from previous instance
+    1 represents an increase from previous instance.
 
-    :param df: The BTC dataset, should be a DataFrame
+    :param df: The dataset, should be a DataFrame
+    :param target: The dataset's target value, should be a str
     :return: df - DataFrame
     """
-    df['Close'] = [0 if df['diff'][i] < 0 else 1 for i in range(len(df['Close']))]
+    if df[target].dtype not in ['float64', 'int64']:
+        logging.warning("The target values are not compatible")
+        return df
+
+    df[target] = [int(df[target][max(0, i - 1)] < df[target][min(len(df[target]) - 1, i)])
+                  for i in range(len(df[target]))]
 
     # Checks for class imbalance
-    if len(df['Close'] == 0) != len(df['Close'] == 1):
+    if len(df[target] == 0) != len(df[target] == 1):
         logging.warning("Class imbalance is present")
 
-    df['Close'] = df['Close'].astype("category")
+    df[target] = df[target].astype("category")
     return df
 
 
-def plotClassifications(y_test: DataFrame, name: str, y_preds: ndarray) -> None:
+def plotClassifications(y_test: Series, name: str, y_preds: ndarray) -> None:
     """
     Plots the predictions in a confusion matrix format.
 
-    :param y_test: Testing dependent variables, should be a DataFrame
+    :param y_test: Testing dependent variables, should be a Series
     :param name: Classifier's name, should be a str
     :param y_preds: Classifier predictions, should be a ndarray
     :return: None
@@ -48,11 +50,11 @@ def plotClassifications(y_test: DataFrame, name: str, y_preds: ndarray) -> None:
     plt.show()
 
 
-def resultAnalysis(y_test: DataFrame, y_pred: ndarray, show: bool = True) -> dict:
+def resultAnalysis(y_test: Series, y_pred: ndarray, show: bool = True) -> dict:
     """
     Calculates and displays the result analysis for classifiers.
 
-    :param y_test: Testing dependent variables, should be a DataFrame
+    :param y_test: Testing dependent variables, should be a Series
     :param y_pred: Model predictions, should be a ndarray
     :param show: Whether to show the results, should be a bool
     :return: results - dict[str: float]
